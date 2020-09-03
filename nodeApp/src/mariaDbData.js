c5d04d1989c1eb0d5733e711dfca59ca7d116e1d
Juan Peredo <jperedo@bolbeck.com>
2020-09-03 17:30:54 -0500

    initial commit

---
+++ nodeApp/src/mariaDbData.js
@@ -0,0 +1,46 @@
+
+const mymariadb = require('mariadb');
+
+
+var pool = mymariadb.createPool({
+    connectionLimit : 5,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DB_NAME
+});
+
+/**
+ * Returns pre-rendered page with daya to client
+ */
+const getData =  (request, response) => {
+  getMariaBbData(request, response).then(result=>{
+    response.render('mydbdata', {
+      title: 'Fine Dinning' ,
+      tableHeader:'Delicious Foods by Maria',
+      dataList: result
+    })
+  });
+}
+
+/**
+ * Get data from DB
+ */
+async function getMariaBbData() {
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    const rows = await conn.query('SELECT * FROM test ORDER BY id ASC');
+    return rows
+  } catch (err) {
+    throw err;
+  }
+  finally {
+    if (conn) { conn.end();}
+  }
+}
+
+module.exports = {getData,
+                  getMariaBbData
+                }
